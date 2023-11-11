4cfc382c5925c3ad932e5f1d887f56d2bd7ea76f
sharad <phs#gmail.com>
2023-11-11 05:55:37 -0500

    neovim nightly

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -87,6 +87,17 @@
   ];
 
 
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+  };
+
   services.udiskie.enable = true;
 
   # ╓──────────────────────────────────────────────────────────────────────────────╖
